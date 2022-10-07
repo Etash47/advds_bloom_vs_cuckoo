180b971ecacfafa313c7995c2bb542e93a6ef3af
Etash47 <etash.kalra@gmail.com>
2022-10-07 15:48:13 -0600

    Finished Bloom filter fxns

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,35 +1,46 @@
+import math
+
 class BloomFilter:
 
     # Params:
-    # n_inserted: number of elements inserted thus far
     # m_bit_v_size: The size of the bit vector we are working with
     # k_hash_fxns: The number of hash functions which an insertion will be hashed amongst
+    # hash_fxns: An array of the hash functions we work with
 
     # Other object attributes:
-    # hash_fxns: An array of the hash functions we work with
+    # n_inserted: number of elements inserted thus far
     # bit_vector: An array of binary values of size m_bit_v_size
 
-    def __init__(self, m_bit_v_size, k_hash_fxns):
-        self.n_inserted = 0
+    def __init__(self, m_bit_v_size, k_hash_fxns, hash_fxns):
         self.m_bit_v_size = m_bit_v_size
         self.k_hash_fxns = k_hash_fxns
-
+        self.hash_fxns = hash_fxns
+        
         self.bit_vector = [0] * self.m_bit_v_size
+        self.n_inserted = 0
 
-        self.hash_fxns = []
-
-    def add_hash_function(self, fxn):
-        if len(self.hash_fxns) >= self.k_hash_fxns:
-            return False
-        else:
-            self.hash_fxns.append(fxn)
-            return True
 
     def insert(self, key):
-        return False
+        for h in self.hash_fxns:
+            hash_val = h(key)
+            self.bit_vector[hash_val] = 1
+        
+        self.n_inserted += 1
+        return True
 
-    def check_with_prob(self, key):
-        return False
+    def check_with_false_prob(self, key):
+        for h in self.hash_fxns:
+            hash_val = h(key)
+            if self.bit_vector[hash_val] == 0:
+                return False, 0
+
+        false_prob = (1 - 
+            math.exp(
+                    float(-self.k_hash_fxns * self.n_inserted) / float(self.m_bit_v_size)
+                )
+            ) ** self.k_hash_fxns
+
+        return True, false_prob
 
 
 
